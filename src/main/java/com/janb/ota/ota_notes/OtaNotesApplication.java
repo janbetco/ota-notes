b38cb27cff1f71c02c878ba4e32b20cf7eac1ec0
Jan Betco <jan.betco@gmail.com>
2024-08-09 17:23:04 +0800

    Initial commit

---
+++ src/main/java/com/janb/ota/ota_notes/OtaNotesApplication.java
@@ -0,0 +1,13 @@
+package com.janb.ota.ota_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtaNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janb/ota/ota_notes/OtaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janb.ota.ota_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
