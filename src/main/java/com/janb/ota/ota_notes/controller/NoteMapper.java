6d73842c24ef92f3abdd647cae4a81aa503cd644
Jan Betco <jan.betco@gmail.com>
2024-08-14 17:55:30 +0800

    Ignore missing field

--- src/main/java/com/janb/ota/ota_notes/controller/NoteMapper.java
+++ src/main/java/com/janb/ota/ota_notes/controller/NoteMapper.java
@@ -4,12 +4,14 @@ import com.janb.ota.ota_notes.model.Note;
 import com.janb.ota.ota_notes.model.NoteRequest;
 import com.janb.ota.ota_notes.model.NoteResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface NoteMapper {
 
   NoteResponse toNoteResponse(Note note);
 
+  @Mapping(target = "id", ignore = true)
   Note toNote(NoteRequest noteRequest);
 
 }
